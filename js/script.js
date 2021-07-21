42d34205e5e0b0c669e8da7aba28364ef4dbe929
Oksana <seregina.oksana.1987@gmail.com>
2021-07-21 04:42:42 +0300

    fix: style button js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-document.querySelector('#item1').onclick = function(){
-    document.querySelector('#text1').classList.toggle('hide');
-}
+document.querySelector("#item1").onclick = function () {
+  document.querySelector("#text1").classList.toggle("hide");
+};
+
+document.querySelector("#item2").onclick = function () {
+  document.querySelector("#text2").classList.toggle("hide");
+};
+
+document.querySelector("#item3").onclick = function () {
+  document.querySelector("#text3").classList.toggle("hide");
+};
+
+document.querySelector("#item4").onclick = function () {
+  document.querySelector("#text4").classList.toggle("hide");
+};
