5500f613d8a3efb59552c67cf3c0247e3410b574
Oksana <seregina.oksana.1987@gmail.com>
2021-11-11 13:53:00 +0300

    add burger menu in index.html

--- js/script.js
+++ js/script.js
@@ -13,3 +13,30 @@ document.querySelector("#item3").onclick = function () {
 document.querySelector("#item4").onclick = function () {
   document.querySelector("#text4").classList.toggle("hide");
 };
+
+//Burger menu
+
+let burgerMenu = document.querySelector("#burger");
+let menuList = document.querySelector(".header-menu");
+let hideImg = document.querySelector(".hide");
+let hideImg1 = document.querySelector(".hide-search");
+let hideUser = document.querySelector(".hide-user");
+let closeBtn = document.querySelector("#close");
+
+function openBurger() {
+  burgerMenu.style.display = "none";
+  menuList.classList.add("show");
+  hideImg.style.position = "static";
+  hideImg1.innerHTML = "<span>Search</span>";
+  hideUser.style.display = "none";
+  closeBtn.style.display = "block";
+}
+
+function closeBurger() {
+  burgerMenu.style.display = "block";
+  menuList.classList.remove("show");
+  closeBtn.style.display = "none";
+}
+
+burgerMenu.addEventListener("click", openBurger);
+closeBtn.addEventListener("click", closeBurger);
