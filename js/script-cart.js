755717afbbc135c9ae9ed1d817604697f648a747
Oksana <seregina.oksana.1987@gmail.com>
2021-11-14 12:09:31 +0300

    add data-atr in counter

--- js/script-cart.js
+++ js/script-cart.js
@@ -2,24 +2,20 @@ let sum = 0;
 
 document.querySelector(".select").addEventListener("click", function (e) {
   let input;
+  console.log(e);
 
-  if (e.target.className === "fas fa-minus") {
+  if (e.target.dataset.action === "remove") {
     e.preventDefault();
-    input = Number(
-      e.target.parentElement.parentElement.nextElementSibling.innerText
-    );
+    input = Number(e.target.parentElement.nextElementSibling.innerText);
     if (input > 1) {
       input--;
-      e.target.parentElement.parentElement.nextElementSibling.innerText = input;
+      e.target.parentElement.nextElementSibling.innerText = input;
     }
-  } else if (e.target.className === "fas fa-plus") {
+  } else if (e.target.dataset.action === "add") {
     e.preventDefault();
-    input = Number(
-      e.target.parentElement.parentElement.previousElementSibling.innerText
-    );
+    input = Number(e.target.parentElement.previousElementSibling.innerText);
     input++;
-    e.target.parentElement.parentElement.previousElementSibling.innerText =
-      input;
+    e.target.parentElement.previousElementSibling.innerText = input;
   } /* else if (e.target.innerText === "ПОСЧИТАТЬ СУММУ ВСЕХ ТОВАРОВ") {
     currentTotalItems.innerText = sum;
   } */
--- js/script-quantity.js
+++ js/script-quantity.js
@@ -2,10 +2,10 @@ let sum = 1;
 
 document.querySelector(".quantity").addEventListener("click", (e) => {
   e.preventDefault();
-  if (e.target.className === "fas fa-minus" && sum > 1) {
+  if (e.target.dataset.action === "remove" && sum > 1) {
     sum--;
     document.querySelector(".one").innerText = sum;
-  } else if (e.target.className === "fas fa-plus") {
+  } else if (e.target.dataset.action === "add") {
     sum++;
     document.querySelector(".one").innerText = sum;
   }
